433c4b535018bc0fb31c71e380daf9c5981a86a3
Mario Nascimento <mrohenrique@gmail.com>
2020-07-29 04:25:59 -0300

    Inicio projeto autenticador forum

---
+++ src/main/java/com/comexport/forumauth/ForumAuthApplication.java
@@ -0,0 +1,13 @@
+package com.comexport.forumauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comexport/forumauth/ForumAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comexport.forumauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
